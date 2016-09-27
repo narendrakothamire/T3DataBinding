50934d271dbae7e3009684e45f31c3117efbf71b
techguy <techguynarendra@gmail.com>
2016-09-27 21:22:47 +0530

    fisrt commit

---
+++ app/src/main/java/com/softwares/swamisamarth/t3databinding/views/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.softwares.swamisamarth.t3databinding.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softwares.swamisamarth.t3databinding.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
